d95f100dc6f79b9f8fae823587105db5464a1f62
SatanTech <140904777+SatanTech@users.noreply.github.com>
2023-08-10 20:38:02 +0700

    Update menu.sh

--- menu/menu.sh
+++ menu/menu.sh
@@ -221,7 +221,7 @@ datediff() {
 }
 mai="datediff "$Exp" "$DATE""
 echo -e " ${BIBlue}┌──────────────────────────────────────────────────────────┐${NC}"
-echo -e " ${BIBlue}│ ${GREEN} Version       : $(cat /opt/.ver) Last Update ${NC}"
+echo -e " ${BIBlue}│ ${GREEN} Version       : $(cat /opt/.ver) Libev ${NC}"
 echo -e " ${BIBlue}│ ${GREEN} User          :\033[1;36m $Name \e[0m"
 echo -e " ${BIBlue}│ ${GREEN} Developer     :\033[1;36m SatanTech \e[0m"
 if [ $exp \< 1000 ];
