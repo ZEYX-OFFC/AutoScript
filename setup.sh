0b69de13d6272c73e120ef8928dd1d54619e1e2a
SatanTech <140904777+SatanTech@users.noreply.github.com>
2023-08-11 09:09:31 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -364,31 +364,35 @@ echo -e " \E[41;1;39m           >>> Install Source <<<          \E[0m$NC"
 echo -e "└─────────────────────────────────────────┘"
 sleep 1 
 wget -q https://raw.githubusercontent.com/SatanTech/Kontol/main/tools/arzsource.sh && chmod +x arzsource.sh && ./arzsource.sh
+clear
 #install ssh-vpn
 echo -e "┌─────────────────────────────────────────┐"
 echo -e " \E[41;1;39m          >>> Install SSH / WS <<<        \E[0m$NC"
 echo -e "└─────────────────────────────────────────┘"
 sleep 1
 wget -q https://raw.githubusercontent.com/SatanTech/Kontol/main/tools/ssh-vpn.sh && chmod +x ssh-vpn.sh && ./ssh-vpn.sh
+clear
 #install ins-xray
 echo -e "┌─────────────────────────────────────────┐"
 echo -e " \E[41;1;39m            >>> Install Xray <<<         \E[0m$NC"
 echo -e "└─────────────────────────────────────────┘"
 sleep 1 
 wget -q https://raw.githubusercontent.com/SatanTech/Kontol/main/tools/ins-xray.sh && chmod +x ins-xray.sh && ./ins-xray.sh
+clear
 #install ins-xray
 echo -e "┌─────────────────────────────────────────┐"
 echo -e " \E[41;1;39m            >>> Install BR <<<           \E[0m$NC"
 echo -e "└─────────────────────────────────────────┘"
 sleep 1 
 wget -q https://raw.githubusercontent.com/SatanTech/Kontol/main/backup/set-br.sh && chmod +x set-br.sh && ./set-br.sh
+clear
 #install ins-xray
 echo -e "┌─────────────────────────────────────────┐"
 echo -e " \E[41;1;39m            >>> Install slowdns <<<           \E[0m$NC"
 echo -e "└─────────────────────────────────────────┘"
 sleep 1 
 wget https://raw.githubusercontent.com/Andyvpn/Autoscript-by-azi/main/autoscript-ssh-slowdns-main/slowdns.sh && chmod +x slowdns.sh && ./slowdns.sh
-
+clear
 # // Download Data
 echo -e "${GREEN}Download Data${NC}"
 wget -q -O /usr/bin/usernew "https://raw.githubusercontent.com/SatanTech/Kontol/main/usernew.sh"
