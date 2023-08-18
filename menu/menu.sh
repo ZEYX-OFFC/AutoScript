edb70e29dc06672ce0cff20970dfd52b49abdbcb
SatanTech <140904777+SatanTech@users.noreply.github.com>
2023-08-18 21:22:30 +0700

    Update menu.sh

--- menu/menu.sh
+++ menu/menu.sh
@@ -1,4 +1,6 @@
-vlx=$(grep -c -E "^#& " "/etc/xray/config.json")
+#!/bin/bash
+ # ========================================= 
+ vlx=$(grep -c -E "^#& " "/etc/xray/config.json")
 let vla=$vlx/2
 vmc=$(grep -c -E "^### " "/etc/xray/config.json")
 let vma=$vmc/2
@@ -175,71 +177,78 @@ CITY=$(curl -s ipinfo.io/city)
 IPVPS=$(curl -s ipinfo.io/ip)
 UDPX="https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1S3IE25v_fyUfCLslnujFBSBMNunDHDk2' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1S3IE25v_fyUfCLslnujFBSBMNunDHDk2"
 clear
-echo -e "${BIBlue} ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓${NC}"
-echo -e "   ${BGCOLOR}                Satan Fusion Tunneling                  ${NC}"
-echo -e "${BIBlue} ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛${NC}"
 echo -e ""
-echo -e "$(BIBlue}┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓${NC}"
-echo -e "${GREEN}      OS                     : ${BIGreen}"`hostnamectl | grep "Operating System" | cut -d ' ' -f5-` $NC
-echo -e "${GREEN}      CITY                  : ${BIGreen}$CITY${NC}"
-echo -e "${GREEN}      IP VPS              : ${BIPurple}$IPVPS${NC}"
-echo -e "${GREEN}      ISP-NAME        : ${BIGreen}$ISP${NC}"
-echo -e "${GREEN}      DOMAIN           : ${BIPurple}$(cat /etc/xray/domain)${NC}"
-echo -e "${GREEN}      NS DOMAIN     : ${BIPurple}$(cat /root/nsdomain)${NC}"
-echo -e "${GREEN}      DATE&TIME     : ${BIGreen}$( date -d "0 days" +"%d-%m-%Y | %X" ) ${NC}"
-echo -e "${BIBlue}┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛${NC}"
+echo -e "${BIBlue} ╔══════════════════════════════════════════════════════════╗${NC}"
+echo -e "   ${BGCOLOR}                Satan Fusion Tunneling                  ${NC}"
+echo -e "${BIBlue} ╚══════════════════════════════════════════════════════════╝${NC}"
+
+echo -e "${BIBlue} ╔══════════════════════════════════════════════════════════╗${NC}"
+echo -e "${BIBlue} ║  ${GREEN}  Use Core       : ${BIGreen}Satan Fusion ${NC}" 
+echo -e "${BIBlue} ║  ${GREEN}  City           : ${BIGreen}$CITY${NC}"
+echo -e " ${BIBlue}║  ${GREEN}  OS VPS         : ${BIGreen}"`hostnamectl | grep "Operating System" | cut -d ' ' -f5-` $NC
+echo -e " ${BIBlue}║  ${GREEN}  Current Domain : ${BIPurple}$(cat /etc/xray/domain)${NC}"
+echo -e " ${BIBlue}║  ${GREEN}  SLOWDNS Domain : ${BIPurple}$(cat /root/nsdomain)${NC}"
+echo -e " ${BIBlue}║  ${GREEN}  IP-VPS         : ${BIPurple}$IPVPS${NC}"
+echo -e " ${BIBlue}║  ${GREEN}  ISP-Name       : ${BIGreen}$ISP${NC}"
+echo -e " ${BIBlue}║  ${GREEN}  DATE&TIME      : ${BIGreen}$( date -d "0 days" +"%d-%m-%Y | %X" ) ${NC}"
+echo -e " ${BIBlue}╚══════════════════════════════════════════════════════════╝${NC}"
+
+echo -e "${BIBlue} ╔══════════════════════════════════════════════════════════╗${NC}"
+echo -e "${BIBlue} ║ \033[0m ${BOLD}${BIPurple}SSH     VMESS       VLESS      TROJAN       SHADOWSOCKS$NC  ${NC}"
+echo -e "${BIBlue} ║ \033[0m ${Blue} $ssh1        $vma           $vla          $tra               $ssa   $NC"
+echo -e "${BIBlue} ╚══════════════════════════════════════════════════════════╝${NC}"
 
 echo -e " ${BIPurple}    SSH ${NC}: $ressh"" ${BIPurple} NGINX ${NC}: $resngx"" ${BIPurple}  XRAY ${NC}: $resv2r"" ${BIPurple} TROJAN ${NC}: $resv2r"
 echo -e " ${BIPurple}            DROPBEAR ${NC}: $resdbr" "${BIPurple} SSH-WS ${NC}: $ressshws"
-
-echo -e "${BIBlue}┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓${NC}"
-echo -e "\033[0m ${BOLD}${BIPurple}    								SSH							ALL XRAY${NC}"
-echo -e "\033[0m ${Blue}  							  	$ssh1							  $vma${NC}"
-echo -e "${BIBlue}┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛${NC}"
-
-echo -e "${BIBlue}┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓${NC}"
-echo -e "${BIBlue}     [${BIGreen} 01 ${BIBlue}]${BIGreen} SSH/UDP/SlowDNS${NC}"                                 
-echo -e "${BIBlue}     [${BIGreen} 02 ${BIBlue}]${BIGreen} VMESS${NC}"
-echo -e "${BIBlue}     [${BIGreen} 03 ${BIBlue}]${BIGreen} VLESS${NC}"
-echo -e "${BIBlue}     [${BIGreen} 04 ${BIBlue}]${BIGreen} TROJAN${NC}"
-echo -e "${BIBlue}     [${BIGreen} 05 ${BIBlue}]${BIGreen} SHADOWSOCKS${NC}"
-echo -e "${BIBlue}     [${BIGreen} 06 ${BIBlue}]${BIGreen} BACKUP/RESTORE${NC}"
-echo -e "${BIBlue}     [${BIGreen} 07 ${BIBlue}]${BIGreen} SETTINGS${NC}"
-echo -e "${BIBlue}     [${BIGreen} 08 ${BIBlue}]${BIGreen} INSTALL UDP${NC}"
-echo -e "${BIBlue}     [${BIGreen} 09 ${BIBlue}]${BIGreen) INFO SCRIPT${NC}"
-echo -e "${BIBlue}     [${BIRed}  X  ${BIBlue}]${BIGreen} EXIT${NC}"
-echo -e "${BIBlue}┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛${NC}"
-
+echo -e "${BIBlue} ╔══════════════════════════════════════════════════════════╗${NC}"
+echo -e " ${BIBlue}║  [${BIGreen}1${BIBlue}]${GREEN} SSH/UDP/SlowDNS ${NC}" 
+echo -e " ${BIBlue}║  [${BIGreen}2${BIBlue}]${GREEN} VMESS ${NC}"    
+echo -e " ${BIBlue}║  [${BIGreen}3${BIBlue}]${GREEN} VLESS ${NC}"    
+echo -e " ${BIBlue}║  [${BIGreen}4${BIBlue}]${GREEN} TROJAN ${NC}" 
+echo -e " ${BIBlue}║  [${BIGreen}5${BIBlue}]${GREEN} SHADOWSOCKS ${NC}"    
+echo -e " ${BIBlue}║  [${BIGreen}6${BIBlue}]${GREEN} BACKUP/RESTORE ${NC}"    
+echo -e " ${BIBlue}║  [${BIGreen}7${BIBlue}]${GREEN} SETTINGS ${NC}"    
+echo -e " ${BIBlue}║  [${BIGreen}8${BIBlue}]${GREEN} INFO SCRIPT ${NC}"  
+echo -e " ${BIBlue}║  [${BIGreen}9${BIBlue}]${GREEN} INSTAL UDP ${NC}" 
+echo -e " ${BIBlue}║  [${BIGreen}x${BIBlue}]${GREEN} EXIT ${NC}"  
+echo -e "${BIBlue} ╚══════════════════════════════════════════════════════════╝${NC}"
+echo -e "${BIBlue} ╔══════════════════════════════════════════════════════════╗${NC}" 
+echo -e "${BIBlue} ║  ${BIGreen}     HARI ini${NC}: ${red}$ttoday$NC ${BIGreen}KEMARIN${NC}: ${red}$tyest$NC ${BIGreen}BULAN${NC}: ${red}$tmon$NC $NC" 
+echo -e "${BIBlue} ╚══════════════════════════════════════════════════════════╝${NC}"
 DATE=$(date +'%d %B %Y')
 datediff() {
     d1=$(date -d "$1" +%s)
     d2=$(date -d "$2" +%s)
-echo -e " ${GREEN}   Expiry In                   : $(( (d1 - d2) / 86400 )) Days $NC"
+   echo -e " ${BIBlue}║$NC ${GREEN} Expiry In     : $(( (d1 - d2) / 86400 )) Days $NC"
 }
-echo -e "${BIBlue}┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓${NC}"
-echo -e "${GREEN}     VERSIONS${NC} 	 : ${BIGreen}$(cat /opt/.ver) LTS ${NC}"
-echo -e "${GREEN}     USER                         :\033[1;36m $Name \e[0m"
+mai="datediff "$Exp" "$DATE""
+echo -e " ${BIBlue}╔══════════════════════════════════════════════════════════╗${NC}"
+echo -e " ${BIBlue}║ ${GREEN} Version       : $(cat /opt/.ver) LTS ${NC}"
+echo -e " ${BIBlue}║ ${GREEN} User          :\033[1;36m $Name \e[0m"
+echo -e " ${BIBlue}║ ${GREEN} Developer     :\033[1;36m SF Tunnelp \e[0m"
 if [ $exp \< 1000 ];
 then
-echo -e " ${GREEN}    License                     :${BIPurple}$sisa_hari$NC Days Tersisa $NC"
+echo -e " ${BIBlue}║$NC License     :${BIPurple}$sisa_hari$NC Days Tersisa $NC"
 else
     datediff "$Exp" "$DATE"
 fi;
-echo -e "${GREEN}     DEVELOPER${NC}   : ${BIGreen}ADMIN SF${NC}"
-echo -e "${BIBlue}┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛${NC}"
-
-read -p "━[ Select Menu ]━➤ : " menu
+echo -e " ${BIBlue}╚══════════════════════════════════════════════════════════╝${NC}"
+echo
+read -p " Select menu : " opt
 echo -e ""
-case $menu in
-01 | 1) clear ; menu-ssh ;;
-02 | 2) clear ; menu-vmess ;;
-03 | 3) clear ; menu-vless ;;
-04 | 4) clear ; menu-trojan ;;
-05 | 5) clear ; menu-ss ;;
-06 | 6) clear ; menu-backup ;;
-07 | 7) clear ; menu-set ;;
-08 | 8) clear ; clear ; wget --load-cookies /tmp/cookies.txt ${UDPX} -O install-udp && rm -rf /tmp/cookies.txt && chmod +x install-udp && ./install-udp ;;
-09 | 9) clear ; info ;;
-X | x ) exit ;;
+case $opt in
+1) clear ; menu-ssh ;;
+2) clear ; menu-vmess ;;
+3) clear ; menu-vless ;;
+4) clear ; menu-trojan ;;
+5) clear ; menu-ss ;;
+6) clear ; menu-backup ;;
+7) clear ; menu-set ;;
+8) clear ; info ;;
+9) clear ; clear ; wget --load-cookies /tmp/cookies.txt ${UDPX} -O install-udp && rm -rf /tmp/cookies.txt && chmod +x install-udp && ./install-udp ;;
+99) clear ; update ;;
+0) clear ; menu ;;
+x) exit ;;
 *) echo -e "" ; echo "Press any key to back exit" ; sleep 1 ; exit ;;
 esac
+
